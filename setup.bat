cab75f91c7ea3f5fae8e2a765024f33703fec60c
Olivier Delalleau <olivier.delalleau@ubisoft.com>
2018-08-15 11:36:38 -0400

    Script to setup environment

---
+++ setup.bat
@@ -0,0 +1,4 @@
+rem Go to https://www.lfd.uci.edu/~gohlke/pythonlibs/ and download the appropriate curses library
+pip install "C:\Users\odelalleau\Downloads\curses-2.2+utf8-cp36-cp36m-win_amd64.whl"
+conda install --yes numpy scipy jupyter matplotlib pillow
+pip install -e .
